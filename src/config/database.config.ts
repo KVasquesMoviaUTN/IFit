83a78c072936c451ba1eacc05c489b184a5703fb
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2025-02-11 16:11:59 -0300

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -12,7 +12,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   // database: 'IFit',
   // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   // url: process.env.DATABASE_URL,
-  url: 'postgresql:neondb_owner:npg_B4jdtyJ9wHSf@ep-fragrant-cherry-a5og2mdt-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
+  url: 'postgresql://neondb_owner:npg_B4jdtyJ9wHSf@ep-fragrant-cherry-a5og2mdt-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
   autoLoadEntities: true,
   synchronize: true,//false en produccion
   ssl: {
