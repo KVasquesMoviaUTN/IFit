701dffcd76bd20d3d614362cbfa6b72a2b23213e
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2025-02-11 17:02:44 -0300

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import { User } from 'mercadopago';
 import { Category } from 'src/products/entities/category.entity';
+import { Presentation } from 'src/products/entities/presentation.entity';
 import { Product } from 'src/products/entities/product.entity';
+import { PaymentMethod } from 'src/sales/entities/payment-method.entity';
+import { SaleDetail } from 'src/sales/entities/sale-detail.entity';
+import { SaleStatus } from 'src/sales/entities/sale-status.entity';
+import { Sale } from 'src/sales/entities/sale.entity';
 
 config();
 
@@ -15,7 +21,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   // url: process.env.DATABASE_URL,
   url: 'postgresql://modofitsql_user:E99nHiIQO9CFgg7cpAhcP0imAW6LuLBy@dpg-culqbran91rc73eh3cog-a.oregon-postgres.render.com/modofitsql',
-  entities: [Product, Category],
+  entities: [Product, Category, Presentation, User, Sale, SaleDetail, SaleStatus, PaymentMethod, ],
   autoLoadEntities: true,
   synchronize: false,//false en produccion
   ssl: {
