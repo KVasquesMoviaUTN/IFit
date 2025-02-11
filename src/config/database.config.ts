323982fd9ca36e783a122095a93bf099725e81a5
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2025-02-11 15:33:25 -0300

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,20 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'kalil',
-  password: 'fattyshady',
-  database: 'IFit',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: false, // Use only for development!
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'kalil',
+  // password: 'fattyshady',
+  // database: 'IFit',
+  // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  url: process.env.DATABASE_URL,
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
