fd456dcb61d75e22edbe99d7b5ee716c5bf8bcb4
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2025-02-11 17:33:58 -0300

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -20,7 +20,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   // database: 'IFit',
   // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   // url: process.env.DATABASE_URL,
-  url: 'postgresql://modofitsql_user:E99nHiIQO9CFgg7cpAhcP0imAW6LuLBy@dpg-culqbran91rc73eh3cog-a.oregon-postgres.render.com/modofitsql',
+  url: 'postgresql://neondb_owner:npg_B4jdtyJ9wHSf@ep-fragrant-cherry-a5og2mdt-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
   entities: [Product, Category, Presentation, User, Sale, SaleDetail, SaleStatus, PaymentMethod, ],
   autoLoadEntities: true,
   synchronize: false,//false en produccion
