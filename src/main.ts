5f542260ed6382395201178b49b2ae9a7a4e0fa0
KVasquesMoviaUTN <kvasquesmovia@frba.utn.edu.ar>
2025-02-12 14:04:23 -0300

    environments

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ import type { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const allowedOrigins = [
-    process.env.FRONTEND_URL,
-    // 'https://modofit-five.vercel.app',
-    'http://localhost:3000',
-  ];
+  const isProduction = process.env.NODE_ENV === 'production';
 
-  const port = process.env.PORT || 3000;
+  const allowedOrigins = isProduction
+    ? ['https://modofit-five.vercel.app']
+    : ['http://localhost:8080'];
 
   app.enableCors({ origin: process.env.DATABASE_URL });
 
@@ -34,6 +32,6 @@ async function bootstrap() {
       credentials: true,
     });
 
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
